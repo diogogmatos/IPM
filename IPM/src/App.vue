5f193c350e98bcf091b8f1fc9991a72306dc5879
Mário Rodrigues <mjsrodrigues4@gmail.com>
2024-04-08 21:26:56 +0100

    :sunrise:

---
+++ IPM/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import './index.css'
+
+</script>
+
+<template>
+  <header>
+    
+  </header>
+  <RouterView />
+</template>
+
---
+++ IPM/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FEA500"
+      }
+    }
+  },
+  plugins: [],
+}
+
